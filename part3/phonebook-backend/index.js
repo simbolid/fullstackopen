1d1b28cd913ba2b44a4eea53b9a0058cb70fbf5a
Daniel Simboli <dansimboli@gmail.com>
2021-02-21 12:33:11 -0500

    Exercises 3.5 - 3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,28 +1,30 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456",
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456",
     
   },
   { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523",
   },
   { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345",
     
   },
   { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122",
   }
 ]
 
@@ -34,13 +36,9 @@ app.get('/api/persons', (request, response) => {
 // retrieve single entry
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id) // request ID is a string, server stores ID as int 
-
   console.log(`GET request for ID ${id}`)
-
   const person = persons.find(person => person.id === id)
-
   console.log(person)
-
   if (person) 
     response.json(person)
   else 
@@ -51,9 +49,7 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id) 
   console.log(`DELETE request for ID ${id}`)
-
   persons = persons.filter(person => person.id !== id)
-
   response.status(204).end() // 204 no content
 }) 
 
@@ -61,6 +57,39 @@ app.delete('/api/persons/:id', (request, response) => {
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p> ${new Date()}`)
 })
+
+// add an entry
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) 
+    return response.status(400).json({ // 400 bad request
+      error: 'data must include both name and number'
+    })
+  
+  if (persons.some(person => person.name === body.name))
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  
+  const person = {
+    id: generateId(), 
+    name: body.name, 
+    number: body.number
+  }
+  console.log(person)
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+// generate a unique ID number between 1 and 1000
+const generateId = () => {
+  let id = 1
+  do {
+    id = Math.ceil(Math.random() * 1000);
+  } while (persons.some(person => person.id === id)) // ensure uniqueness
+  return id
+}
 
 const PORT = 3001
 app.listen(PORT, () => {

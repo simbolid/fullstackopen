9f73dee35a91cf2933dff560eb504a413e7b0039
Daniel Simboli <dansimboli@gmail.com>
2021-06-08 10:31:38 -0400

    added .env for mongoDB connection

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,40 +1,41 @@
-const mongoose = require('mongoose');
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 // check command line arguments 
-if (process.argv.length < 3 || process.argv.length === 4) {
-  console.log("Usage: node mongo.js [password] [name] [number]");
-  process.exit(1);
+if (process.argv.length === 3) {
+  console.log("Usage: node mongo.js [name] [number]")
+  console.log("Provide name along with number")
+  process.exit(1)
 }
 
 // connect to MongoDB cluster
-const password = process.argv[2];
-const url = `mongodb+srv://fullstack:${password}@cluster0.9kcqq.mongodb.net/note-app?retryWrites=true&w=majority`;
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+const url = process.env.DB_URI
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String
-});
+})
 
-const Person = new mongoose.model('Person', personSchema);
+const Person = new mongoose.model('Person', personSchema)
 
-// if only password provided, display phonebook entries
-if (process.argv.length === 3) {
+// if no entry given, display phonebook entries
+if (process.argv.length === 2) {
   Person.find({}).then(result => {
     console.log('Phonebook:')
     result.forEach(person => { 
-      console.log(`${person.name} ${person.number}`); 
-    });
-    mongoose.connection.close();
-  });
+      console.log(`${person.name} ${person.number}`) 
+    })
+    mongoose.connection.close()
+  })
 }
 // otherwise, add the given entry to the database
 else {
   const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
-  });
+    name: process.argv[2],
+    number: process.argv[3]
+  })
   person.save().then(() => { 
     mongoose.connection.close(); 
-  });
+  })
 }

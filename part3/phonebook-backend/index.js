1436bf3aa10c424bc74d1913fe7351828138eaa2
Daniel Simboli <dansimboli@gmail.com>
2021-06-11 10:46:39 -0400

    Exercise 3.16: error handling

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,12 +5,12 @@ const Person = require('./models/person') // mongoose model
 
 const app = express()
 
-// parse requests containing JSON payloads
-app.use(express.json())
-
 // upon receiving GET request, check if build directory contains a file
 // corresponding to request address
 app.use(express.static('build'))  
+
+// parse requests containing JSON payloads
+app.use(express.json())
 
 // HTTP request logging 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
@@ -20,39 +20,36 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 /* Route Handlers */
 
 // retrieve all entries
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(people => response.json(people))
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(people => response.json(people))
+    .catch(error => next(error))
 })
 
 // retrieve single entry
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-    .then(note => { response.json(note) })
-    .catch(error => {
-      console.log(error)
-      response.status(404).end() // 404 not found
-    })
+    .then(person => person ? response.json(person) : response.status(404).end()) // if person not found, return 404 not found 
+    .catch(error => next(error))
 })
 
 // retrieve phonebook info: number of people, datetime
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Person.count({}, (err, count) => {
     response.send(`<p>Phonebook has info for ${count} people</p> ${new Date()}`)
   })
+  .catch(error => next(error))
 })
 
 // delete an entry
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(person => {
-      person ? response.json(person) 
-             : response.status(404).end()
-    })
-    // .catch(error => next(error))
+    .then(person => person ? response.json(person) : response.status(404).end())  
+    .catch(error => next(error))
 })  
 
 // add an entry
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) 
@@ -71,9 +68,24 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => response.json(savedPerson))
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 
+/* End route handlers */
+
+// see https://mongoosejs.com/docs/api/error.html for mongoose error types
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'malformatted id' }) // 400 bad request 
+
+  next(error) // call express' default error handler 
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

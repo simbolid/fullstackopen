a4f03b0709dfdb8f9e6180e02e35e1cb2df9c08b
Daniel Simboli <dansimboli@gmail.com>
2021-06-17 11:16:08 -0400

    Exercise 4.2 - added blog router

---
+++ part4/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,24 @@
+const blogRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+// get all blogs
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
+
+// add blog
+blogRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    });
+});
+
+module.exports = blogRouter;

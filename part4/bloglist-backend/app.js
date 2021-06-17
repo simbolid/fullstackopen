09ed94d87fabedff5873e0f0271ed8c55675630a
Daniel Simboli <dansimboli@gmail.com>
2021-06-17 11:35:59 -0400

    Exercise 4.2 - added app.js

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+const app = express();
+
+// connect to MongoDB
+logger.info('connecting to MongoDB');
+mongoose.connect(config.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => logger.error('connected to MongoDB'))
+  .catch((error) => logger.error('error connecting to MongoDB:', error.message));
+
+// allow requests from any origin
+app.use(cors());
+
+// parse requests with JSON payloads
+app.use(express.json());
+
+// route handling
+app.use('/api/blogs', blogRouter);
+
+module.exports = app;

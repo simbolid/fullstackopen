45e53f242b7b3d9c291a3bbe37b5bb428c7ea18d
Daniel Simboli <dansimboli@gmail.com>
2021-08-01 13:23:55 -0400

    5.7 - add button for viewing the details of a blog

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,8 +1,54 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => { 
+  const [open, setOpen] = useState(false);
+
+  const blogStyle = {
+    display: "inline-block",
+    paddingBlock: 10,
+    paddingInline: 5,
+    marginBottom: 5,
+    border: 'solid',
+    borderColor: 'gray',
+    borderWidth: open ? 1 : 0,
+    borderRadius: 5,
+  };
+
+  const showWhenOpen = { display: open ? "" : "none" };
+  const buttonSpacing = { 
+    marginLeft: 4, 
+    width: 40,
+  };
+  
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <div style={blogStyle}>
+        <span>
+          <em>{`${blog.title} `}</em> {`by ${blog.author}`}
+          <button 
+            style={buttonSpacing}
+            onClick={toggleOpen}
+          >
+            { open ? "Close" : "View" }
+          </button>
+        </span>
+        <div style={showWhenOpen}>
+          {blog.url}
+          <br></br>
+          Likes: {blog.likes}
+          <button style={buttonSpacing}>
+            Like
+          </button>
+        </div>
+      </div>
+      <br></br>
+    </>
+  );
+};
 
 export default Blog;

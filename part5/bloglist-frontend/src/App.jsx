f35eb33f9bfa06ba628c6fcdf5a58fa1d6db7f2a
Daniel Simboli <dansimboli@gmail.com>
2021-08-01 20:19:03 -0400

    5.9 - order blogs by likes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -124,7 +124,9 @@ const App = () => {
         <BlogForm createBlog={createBlog} ref={blogFormRef} />
       </Togglable>
       <br></br>
-      {blogs.map((blog) => (
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
       ))}
     </>
